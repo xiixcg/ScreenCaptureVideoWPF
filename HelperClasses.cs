7cd661b3c3c74e86e51a7a4629bca834177a1971
young <badfriend87@hotmail.com>
2025-01-16 22:57:55 -0800

    Add project files.

---
+++ HelperClasses.cs
@@ -0,0 +1,287 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace SreenCaptureVideoWPF {
+//	internal class HelperClasses {
+//	}
+//}
+
+
+using System.Runtime.InteropServices;
+using System;
+using Windows.Graphics.DirectX.Direct3D11;
+using Windows.Graphics;
+//using static MonitorEnumerationHelper;
+using System.Collections.Generic;
+using System.Numerics;
+using Windows.Foundation;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Graphics.Capture;
+
+[ComImport]
+[Guid("A9B3D012-3DF2-4EE3-B8D1-8695F457D3C1")]
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+[ComVisible(true)]
+interface IDirect3DDxgiInterfaceAccess {
+	IntPtr GetInterface([In] ref Guid iid);
+};
+
+public static class Direct3D11Helpers {
+	internal static Guid IInspectable = new Guid("AF86E2E0-B12D-4c6a-9C5A-D7AA65101E90");
+	internal static Guid ID3D11Resource = new Guid("dc8e63f3-d12b-4952-b47b-5e45026a862d");
+	internal static Guid IDXGIAdapter3 = new Guid("645967A4-1392-4310-A798-8053CE3E93FD");
+	internal static Guid ID3D11Device = new Guid("db6f6ddb-ac77-4e88-8253-819df9bbf140");
+	internal static Guid ID3D11Texture2D = new Guid("6f15aaf2-d208-4e89-9ab4-489535d34f9c");
+
+	[DllImport(
+		"d3d11.dll",
+		EntryPoint = "CreateDirect3D11DeviceFromDXGIDevice",
+		SetLastError = true,
+		CharSet = CharSet.Unicode,
+		ExactSpelling = true,
+		CallingConvention = CallingConvention.StdCall
+		)]
+	internal static extern UInt32 CreateDirect3D11DeviceFromDXGIDevice(IntPtr dxgiDevice, out IntPtr graphicsDevice);
+
+	[DllImport(
+		"d3d11.dll",
+		EntryPoint = "CreateDirect3D11SurfaceFromDXGISurface",
+		SetLastError = true,
+		CharSet = CharSet.Unicode,
+		ExactSpelling = true,
+		CallingConvention = CallingConvention.StdCall
+		)]
+	internal static extern UInt32 CreateDirect3D11SurfaceFromDXGISurface(IntPtr dxgiSurface, out IntPtr graphicsSurface);
+
+	public static IDirect3DDevice CreateD3DDevice() {
+		return CreateD3DDevice(false);
+	}
+
+	public static IDirect3DDevice CreateD3DDevice(bool useWARP) {
+		var d3dDevice = new SharpDX.Direct3D11.Device(
+			useWARP ? SharpDX.Direct3D.DriverType.Software : SharpDX.Direct3D.DriverType.Hardware,
+			SharpDX.Direct3D11.DeviceCreationFlags.BgraSupport);
+		IDirect3DDevice device = null;
+
+		// Acquire the DXGI interface for the Direct3D device.
+		using(var dxgiDevice = d3dDevice.QueryInterface<SharpDX.DXGI.Device3>()) {
+			// Wrap the native device using a WinRT interop object.
+			uint hr = CreateDirect3D11DeviceFromDXGIDevice(dxgiDevice.NativePointer, out IntPtr pUnknown);
+
+			if(hr == 0) {
+				device = Marshal.GetObjectForIUnknown(pUnknown) as IDirect3DDevice;
+				Marshal.Release(pUnknown);
+			}
+		}
+
+		return device;
+	}
+
+
+	internal static IDirect3DSurface CreateDirect3DSurfaceFromSharpDXTexture(SharpDX.Direct3D11.Texture2D texture) {
+		IDirect3DSurface surface = null;
+
+		// Acquire the DXGI interface for the Direct3D surface.
+		using(var dxgiSurface = texture.QueryInterface<SharpDX.DXGI.Surface>()) {
+			// Wrap the native device using a WinRT interop object.
+			uint hr = CreateDirect3D11SurfaceFromDXGISurface(dxgiSurface.NativePointer, out IntPtr pUnknown);
+
+			if(hr == 0) {
+				surface = Marshal.GetObjectForIUnknown(pUnknown) as IDirect3DSurface;
+				Marshal.Release(pUnknown);
+			}
+		}
+
+		return surface;
+	}
+
+
+
+	internal static SharpDX.Direct3D11.Device CreateSharpDXDevice(IDirect3DDevice device) {
+		var access = (IDirect3DDxgiInterfaceAccess) device;
+		var d3dPointer = access.GetInterface(ID3D11Device);
+		var d3dDevice = new SharpDX.Direct3D11.Device(d3dPointer);
+		return d3dDevice;
+	}
+
+	internal static SharpDX.Direct3D11.Texture2D CreateSharpDXTexture2D(IDirect3DSurface surface) {
+		var access = (IDirect3DDxgiInterfaceAccess) surface;
+		var d3dPointer = access.GetInterface(ID3D11Texture2D);
+		var d3dSurface = new SharpDX.Direct3D11.Texture2D(d3dPointer);
+		return d3dSurface;
+	}
+
+
+	public static SharpDX.Direct3D11.Texture2D InitializeComposeTexture(
+		SharpDX.Direct3D11.Device sharpDxD3dDevice,
+		SizeInt32 size) {
+		var description = new SharpDX.Direct3D11.Texture2DDescription {
+			Width = size.Width,
+			Height = size.Height,
+			MipLevels = 1,
+			ArraySize = 1,
+			Format = SharpDX.DXGI.Format.B8G8R8A8_UNorm,
+			SampleDescription = new SharpDX.DXGI.SampleDescription() {
+				Count = 1,
+				Quality = 0
+			},
+			Usage = SharpDX.Direct3D11.ResourceUsage.Default,
+			BindFlags = SharpDX.Direct3D11.BindFlags.ShaderResource | SharpDX.Direct3D11.BindFlags.RenderTarget,
+			CpuAccessFlags = SharpDX.Direct3D11.CpuAccessFlags.None,
+			OptionFlags = SharpDX.Direct3D11.ResourceOptionFlags.None
+		};
+		var composeTexture = new SharpDX.Direct3D11.Texture2D(sharpDxD3dDevice, description);
+
+
+		using(var renderTargetView = new SharpDX.Direct3D11.RenderTargetView(sharpDxD3dDevice, composeTexture)) {
+			sharpDxD3dDevice.ImmediateContext.ClearRenderTargetView(renderTargetView, new SharpDX.Mathematics.Interop.RawColor4(0, 0, 0, 1));
+		}
+
+		return composeTexture;
+	}
+}
+
+class MultithreadLock : IDisposable {
+	public MultithreadLock(SharpDX.Direct3D11.Multithread multithread) {
+		_multithread = multithread;
+		_multithread?.Enter();
+	}
+
+	public void Dispose() {
+		_multithread?.Leave();
+		_multithread = null;
+	}
+
+	private SharpDX.Direct3D11.Multithread _multithread;
+}
+
+public sealed class SurfaceWithInfo : IDisposable {
+	public IDirect3DSurface Surface { get; internal set; }
+	public TimeSpan SystemRelativeTime { get; internal set; }
+
+	public void Dispose() {
+		Surface?.Dispose();
+		Surface = null;
+	}
+}
+
+public class MonitorInfo {
+	public bool IsPrimary { get; set; }
+	public Vector2 ScreenSize { get; set; }
+	public Rect MonitorArea { get; set; }
+	public Rect WorkArea { get; set; }
+	public string DeviceName { get; set; }
+	public IntPtr Hmon { get; set; }
+}
+
+public static class MonitorEnumerationHelper {
+	delegate bool EnumMonitorsDelegate(IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData);
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct RECT {
+		public int left;
+		public int top;
+		public int right;
+		public int bottom;
+	}
+
+	private const int CCHDEVICENAME = 32;
+	[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+	internal struct MonitorInfoEx {
+		public int Size;
+		public RECT Monitor;
+		public RECT WorkArea;
+		public uint Flags;
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHDEVICENAME)]
+		public string DeviceName;
+	}
+
+	[DllImport("user32.dll")]
+	static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip, EnumMonitorsDelegate lpfnEnum, IntPtr dwData);
+
+	[DllImport("user32.dll", CharSet = CharSet.Auto)]
+	static extern bool GetMonitorInfo(IntPtr hMonitor, ref MonitorInfoEx lpmi);
+
+	public static IEnumerable<MonitorInfo> GetMonitors() {
+		var result = new List<MonitorInfo>();
+
+		EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero,
+			delegate (IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData) {
+				MonitorInfoEx mi = new MonitorInfoEx();
+				mi.Size = Marshal.SizeOf(mi);
+				bool success = GetMonitorInfo(hMonitor, ref mi);
+				if(success) {
+					var info = new MonitorInfo {
+						ScreenSize = new Vector2(mi.Monitor.right - mi.Monitor.left, mi.Monitor.bottom - mi.Monitor.top),
+						MonitorArea = new Rect(mi.Monitor.left, mi.Monitor.top, mi.Monitor.right - mi.Monitor.left, mi.Monitor.bottom - mi.Monitor.top),
+						WorkArea = new Rect(mi.WorkArea.left, mi.WorkArea.top, mi.WorkArea.right - mi.WorkArea.left, mi.WorkArea.bottom - mi.WorkArea.top),
+						IsPrimary = mi.Flags > 0,
+						Hmon = hMonitor,
+						DeviceName = mi.DeviceName
+					};
+					result.Add(info);
+				}
+				return true;
+			}, IntPtr.Zero);
+		return result;
+	}
+}
+
+// Seems like this com object require special treatment to be used in UWP project
+
+//public static class GraphicsCaptureHelper {
+//	static readonly Guid GraphicsCaptureItemGuid = new Guid("79C3F95B-31F7-4EC2-A464-632EF5D30760");
+
+//	[ComImport]
+//	[System.Runtime.InteropServices.Guid("3E68D4BD-7135-4D10-8018-9FB6D9F33FA1")]
+//	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+//	[ComVisible(true)]
+//	interface IInitializeWithWindow {
+//		void Initialize(
+//			IntPtr hwnd);
+//	}
+
+//	[ComImport]
+//	[System.Runtime.InteropServices.Guid("3628E81B-3CAC-4C60-B7F4-23CE0E0C3356")]
+//	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+//	[ComVisible(true)]
+//	interface IGraphicsCaptureItemInterop {
+//		IntPtr CreateForWindow(
+//			[In] IntPtr window,
+//			[In] ref Guid iid);
+
+//		IntPtr CreateForMonitor(
+//			[In] IntPtr monitor,
+//			[In] ref Guid iid);
+//	}
+
+//	public static void SetWindow(this GraphicsCapturePicker picker, IntPtr hwnd) {
+//		var interop = (IInitializeWithWindow) (object) picker;
+//		interop.Initialize(hwnd);
+//	}
+
+//	public static GraphicsCaptureItem CreateItemForWindow(IntPtr hwnd) {
+//		var factory = WindowsRuntimeMarshal.GetActivationFactory(typeof(GraphicsCaptureItem));
+//		var interop = (IGraphicsCaptureItemInterop) factory;
+//		var temp = typeof(GraphicsCaptureItem);
+//		var itemPointer = interop.CreateForWindow(hwnd, GraphicsCaptureItemGuid);
+//		var item = Marshal.GetObjectForIUnknown(itemPointer) as GraphicsCaptureItem;
+//		Marshal.Release(itemPointer);
+
+//		return item;
+//	}
+
+//	public static GraphicsCaptureItem CreateItemForMonitor(IntPtr hmon) {
+//		var factory = WindowsRuntimeMarshal.GetActivationFactory(typeof(GraphicsCaptureItem));
+//		var interop = (IGraphicsCaptureItemInterop) factory;
+//		var temp = typeof(GraphicsCaptureItem);
+//		var itemPointer = interop.CreateForMonitor(hmon, GraphicsCaptureItemGuid);
+//		var item = Marshal.GetObjectForIUnknown(itemPointer) as GraphicsCaptureItem;
+//		Marshal.Release(itemPointer);
+
+//		return item;
+//	}
+//}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using Windows.Graphics.Capture;
+using Windows.Graphics.DirectX.Direct3D11;
+using Windows.UI.Composition;
+using Windows.Graphics.DirectX;
+using SharpDX.Direct3D11;
+using Windows.Media.MediaProperties;
+using Windows.Media.Core;
+using Windows.Media.Transcoding;
+using System.Threading;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace SreenCaptureVideoWPF {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		// Capture API objects.
+		private List<GraphicsCaptureItem> _items;
+		private Direct3D11CaptureFramePool _framePool;
+		private GraphicsCaptureSession _session;
+
+		// Non-API related members.
+		private CompositionGraphicsDevice _compositionGraphicsDevice;
+		private Compositor _compositor;
+		private CompositionDrawingSurface _surface;
+
+		private IDirect3DDevice _device;
+		private Device _sharpDxD3dDevice;
+		private List<GraphicsCaptureItem> _captureItems;
+		private GraphicsCaptureItem _captureItem;
+		private Texture2D _composeTexture;
+		private RenderTargetView _composeRenderTargetView;
+		private MediaEncodingProfile _encodingProfile;
+		private VideoStreamDescriptor _videoDescriptor;
+		private MediaStreamSource _mediaStreamSource;
+		private MediaTranscoder _transcoder;
+		private bool _isRecording;
+		private bool _closed;
+		private Multithread _multithread;
+		private ManualResetEvent _frameEvent;
+		private ManualResetEvent _closedEvent;
+		private ManualResetEvent[] _events;
+		private Direct3D11CaptureFrame _currentFrame;
+
+		public MainWindow() {
+			InitializeComponent();
+			if(_items == null) {
+				_items = new List<GraphicsCaptureItem>();
+			}
+			Task setup = SetupEncoding();
+		}
+
+		//// TODO: having trouble getting monitors due to comimport issue with UWP
+		//private List<GraphicsCaptureItem> getAllMonitorsAsItemsToCapture() {
+		//	List<GraphicsCaptureItem> graphicCaptureItems = new List<GraphicsCaptureItem>();
+		//	foreach(MonitorInfo monitor in MonitorEnumerationHelper.GetMonitors()) {
+		//		GraphicsCaptureItem item = GraphicsCaptureHelper.CreateItemForMonitor(monitor.Hmon
+		//			);
+		//		if(item == null)
+		//			continue;
+		//		graphicCaptureItems.Add(item);
+		//	}
+
+		//	return graphicCaptureItems;
+		//}
+
+		private async Task SetupEncoding() {
+			if(!GraphicsCaptureSession.IsSupported()) {
+				// Show message to user that screen capture is unsupported
+				return;
+			}
+
+			// Create the D3D device and SharpDX device
+			if(_device == null) {
+				_device = Direct3D11Helpers.CreateD3DDevice();
+			}
+			if(_sharpDxD3dDevice == null) {
+				_sharpDxD3dDevice = Direct3D11Helpers.CreateSharpDXDevice(_device);
+			}
+
+			try {
+				//Let the user pick an item to capture
+				var picker = new GraphicsCapturePicker();
+				_captureItem = await picker.PickSingleItemAsync();
+				if(_captureItem == null) {
+					return;
+				}
+
+				// Initialize a blank texture and render target view for copying frames, using the same size as the capture item
+				_composeTexture = Direct3D11Helpers.InitializeComposeTexture(_sharpDxD3dDevice, _captureItem.Size);
+				_composeRenderTargetView = new SharpDX.Direct3D11.RenderTargetView(_sharpDxD3dDevice, _composeTexture);
+
+				// This example encodes video using the item's actual size.
+				var width = (uint) _captureItem.Size.Width;
+				var height = (uint) _captureItem.Size.Height;
+
+				// Make sure the dimensions are are even. Required by some encoders.
+				width = (width % 2 == 0) ? width : width + 1;
+				height = (height % 2 == 0) ? height : height + 1;
+
+
+				var temp = MediaEncodingProfile.CreateMp4(VideoEncodingQuality.HD1080p);
+				var bitrate = temp.Video.Bitrate;
+				uint framerate = 30;
+
+				_encodingProfile = new MediaEncodingProfile();
+				_encodingProfile.Container.Subtype = "MPEG4";
+				_encodingProfile.Video.Subtype = "H264";
+				_encodingProfile.Video.Width = width;
+				_encodingProfile.Video.Height = height;
+				_encodingProfile.Video.Bitrate = bitrate;
+				_encodingProfile.Video.FrameRate.Numerator = framerate;
+				_encodingProfile.Video.FrameRate.Denominator = 1;
+				_encodingProfile.Video.PixelAspectRatio.Numerator = 1;
+				_encodingProfile.Video.PixelAspectRatio.Denominator = 1;
+
+				var videoProperties = VideoEncodingProperties.CreateUncompressed(MediaEncodingSubtypes.Bgra8, width, height);
+				_videoDescriptor = new VideoStreamDescriptor(videoProperties);
+
+				// Create our MediaStreamSource
+				_mediaStreamSource = new MediaStreamSource(_videoDescriptor);
+				_mediaStreamSource.BufferTime = TimeSpan.FromSeconds(0);
+				_mediaStreamSource.Starting += OnMediaStreamSourceStarting;
+				_mediaStreamSource.SampleRequested += OnMediaStreamSourceSampleRequested;
+
+				// Create our transcoder
+				_transcoder = new MediaTranscoder();
+				_transcoder.HardwareAccelerationEnabled = true;
+
+
+				// Create a destination file - Access to the VideosLibrary requires the "Videos Library" capability
+				var folder = KnownFolders.SavedPictures;
+				//var folder = KnownFolders.VideosLibrary;
+				var name = DateTime.Now.ToString("yyyyMMddHHmmss");
+				var file = await folder.CreateFileAsync($"{name}.mp4");
+
+				using(var stream = await file.OpenAsync(FileAccessMode.ReadWrite))
+
+					await EncodeAsync(stream);
+
+			}
+			catch(Exception ex) {
+
+				return;
+			}
+		}
+
+		private async Task EncodeAsync(IRandomAccessStream stream) {
+			while(!_isRecording) {
+				_isRecording = true;
+
+				StartCapture();
+
+				var transcode = await _transcoder.PrepareMediaStreamSourceTranscodeAsync(_mediaStreamSource, stream, _encodingProfile);
+
+				await transcode.TranscodeAsync();
+			}
+		}
+
+		private void OnMediaStreamSourceSampleRequested(MediaStreamSource sender, MediaStreamSourceSampleRequestedEventArgs args) {
+			if(_isRecording && !_closed) {
+				try {
+					using(var frame = WaitForNewFrame()) {
+						if(frame == null) {
+							args.Request.Sample = null;
+							StopCapture();
+							Cleanup();
+							return;
+						}
+
+						var timeStamp = frame.SystemRelativeTime;
+
+						var sample = MediaStreamSample.CreateFromDirect3D11Surface(frame.Surface, timeStamp);
+						args.Request.Sample = sample;
+					}
+				}
+				catch(Exception e) {
+					Debug.WriteLine(e.Message);
+					Debug.WriteLine(e.StackTrace);
+					Debug.WriteLine(e);
+					args.Request.Sample = null;
+					StopCapture();
+					Cleanup();
+				}
+			}
+			else {
+				args.Request.Sample = null;
+				StopCapture();
+				Cleanup();
+			}
+		}
+
+		private void OnMediaStreamSourceStarting(MediaStreamSource sender, MediaStreamSourceStartingEventArgs args) {
+			using(var frame = WaitForNewFrame()) {
+				args.Request.SetActualStartPosition(frame.SystemRelativeTime);
+			}
+		}
+
+		public void StartCapture() {
+
+			_multithread = _sharpDxD3dDevice.QueryInterface<SharpDX.Direct3D11.Multithread>();
+			_multithread.SetMultithreadProtected(true);
+			_frameEvent = new ManualResetEvent(false);
+			_closedEvent = new ManualResetEvent(false);
+			_events = new[] { _closedEvent, _frameEvent };
+
+			_captureItem.Closed += OnClosed;
+			_framePool = Direct3D11CaptureFramePool.CreateFreeThreaded(
+				_device,
+				DirectXPixelFormat.B8G8R8A8UIntNormalized,
+				1,
+				_captureItem.Size);
+			_framePool.FrameArrived += OnFrameArrived;
+			_session = _framePool.CreateCaptureSession(_captureItem);
+			_session.StartCapture();
+		}
+
+		private void OnFrameArrived(Direct3D11CaptureFramePool sender, object args) {
+			_currentFrame = sender.TryGetNextFrame();
+			_frameEvent.Set();
+		}
+
+		private void OnClosed(GraphicsCaptureItem sender, object args) {
+			_closedEvent.Set();
+		}
+
+		public SurfaceWithInfo WaitForNewFrame() {
+			// Let's get a fresh one.
+			_currentFrame?.Dispose();
+			_frameEvent.Reset();
+
+			var signaledEvent = _events[WaitHandle.WaitAny(_events)];
+			if(signaledEvent == _closedEvent) {
+				Cleanup();
+				return null;
+			}
+
+			var result = new SurfaceWithInfo();
+			result.SystemRelativeTime = _currentFrame.SystemRelativeTime;
+			using(var multithreadLock = new MultithreadLock(_multithread))
+			using(var sourceTexture = Direct3D11Helpers.CreateSharpDXTexture2D(_currentFrame.Surface)) {
+
+				_sharpDxD3dDevice.ImmediateContext.ClearRenderTargetView(_composeRenderTargetView, new SharpDX.Mathematics.Interop.RawColor4(0, 0, 0, 1));
+
+				var width = MathClamp(_currentFrame.ContentSize.Width, 0, _currentFrame.Surface.Description.Width);
+				var height = MathClamp(_currentFrame.ContentSize.Height, 0, _currentFrame.Surface.Description.Height);
+				var region = new SharpDX.Direct3D11.ResourceRegion(0, 0, 0, width, height, 1);
+				_sharpDxD3dDevice.ImmediateContext.CopySubresourceRegion(sourceTexture, 0, region, _composeTexture, 0);
+
+				var description = sourceTexture.Description;
+				description.Usage = SharpDX.Direct3D11.ResourceUsage.Default;
+				description.BindFlags = SharpDX.Direct3D11.BindFlags.ShaderResource | SharpDX.Direct3D11.BindFlags.RenderTarget;
+				description.CpuAccessFlags = SharpDX.Direct3D11.CpuAccessFlags.None;
+				description.OptionFlags = SharpDX.Direct3D11.ResourceOptionFlags.None;
+
+				using(var copyTexture = new SharpDX.Direct3D11.Texture2D(_sharpDxD3dDevice, description)) {
+					_sharpDxD3dDevice.ImmediateContext.CopyResource(_composeTexture, copyTexture);
+					result.Surface = Direct3D11Helpers.CreateDirect3DSurfaceFromSharpDXTexture(copyTexture);
+				}
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// Returns value clamped to the inclusive range of min and max.
+		/// </summary>
+		/// <param name="value"></param>
+		/// <param name="min"></param>
+		/// <param name="max"></param>
+		/// <returns> min if value less than min. max if value more than max. Else, value </returns>
+		private int MathClamp(int value, int min, int max) {
+			return (value < min) ? min : ((value > max) ? max : value);
+		}
+
+		private void StopCapture() {
+			_closedEvent.Set();
+		}
+
+		private void Cleanup() {
+			_framePool?.Dispose();
+			_session?.Dispose();
+			if(_captureItem != null) {
+				_captureItem.Closed -= OnClosed;
+			}
+			_captureItem = null;
+			_device = null;
+			_sharpDxD3dDevice = null;
+			_composeTexture?.Dispose();
+			_composeTexture = null;
+			_composeRenderTargetView?.Dispose();
+			_composeRenderTargetView = null;
+			_currentFrame?.Dispose();
+		}
+
+		private void StartCaptureButton_Click(object sender, RoutedEventArgs e) {
+			// TODO: make it work with button
+			//StartCapture();
+		}
+		private void StopCaptureButton_Click(object sender, RoutedEventArgs e) {
+			StopCapture();
+		}
+	}
+}
